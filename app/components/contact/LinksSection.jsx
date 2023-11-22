1f9546469a628c1a67112dbb202ebc3aab8cb767
Randel Humphrey <randelhumphrey@gmail.com>
2023-11-22 02:12:39 -0500

    almost finished footer

--- app/components/contact/LinksSection.jsx
+++ app/components/contact/LinksSection.jsx
@@ -4,72 +4,72 @@ const LinksSection = () => {
   return (
     <div className='flex justify-between mt-6'>
       <div>
-        <h2 className='text-sm'>Vehicles</h2>
+        <h2 className='text-sm font-bold'>Vehicles</h2>
         <ul>
-          <li><a href="" className='text-black'>Civic Models</a></li>
-          <li><a href="" className='text-black'>Accord Models</a></li>
-          <li><a href="" className='text-black'>HR-V Crossover</a></li>
-          <li><a href="" className='text-black'>CR-V Crossover</a></li>
-          <li><a href="" className='text-black'>Passport SUV</a></li>
-          <li><a href="" className='text-black'>Pilot SUV</a></li>
-          <li><a href="" className='text-black'>Odyssey Minivan</a></li>
-          <li><a href="" className='text-black'>Ridgeline Truck</a></li>
-          <li><a href="" className='text-black'>Future Vehicles</a></li>
-          <li><a href="" className='text-black'>Rugged Capability</a></li>
-          <li><a href="" className='text-black'>Honda Certified Pre-Owned</a></li>
-          <li><a href="" className='text-black'>All Vehicles</a></li>
+          <li><a href="" className='text-black no-underline'>Civic Models</a></li>
+          <li><a href="" className='text-black no-underline'>Accord Models</a></li>
+          <li><a href="" className='text-black no-underline'>HR-V Crossover</a></li>
+          <li><a href="" className='text-black no-underline'>CR-V Crossover</a></li>
+          <li><a href="" className='text-black no-underline'>Passport SUV</a></li>
+          <li><a href="" className='text-black no-underline'>Pilot SUV</a></li>
+          <li><a href="" className='text-black no-underline'>Odyssey Minivan</a></li>
+          <li><a href="" className='text-black no-underline'>Ridgeline Truck</a></li>
+          <li><a href="" className='text-black no-underline'>Future Vehicles</a></li>
+          <li><a href="" className='text-black no-underline'>Rugged Capability</a></li>
+          <li><a href="" className='text-black no-underline'>Honda Certified Pre-Owned</a></li>
+          <li><a href="" className='text-black no-underline'>All Vehicles</a></li>
         </ul>
       </div>
       <div>
-        <h2 className='text-sm'>Shopping Tools</h2>
+        <h2 className='text-sm font-bold'>Shopping Tools</h2>
         <ul>
-          <li><a href="" className='text-black'>Build & Price</a></li>
-          <li><a href="" className='text-black'>See Offers</a></li>
-          <li><a href="" className='text-black'>Search Inventory</a></li>
-          <li><a href="" className='text-black'>Estimate Payments</a></li>
-          <li><a href="" className='text-black'>Apply for Financing</a></li>
-          <li><a href="" className='text-black'>Trade-in Value</a></li>
-          <li><a href="" className='text-black'>Lease or Finance</a></li>
+          <li><a href="" className='text-black no-underline'>Build & Price</a></li>
+          <li><a href="" className='text-black no-underline'>See Offers</a></li>
+          <li><a href="" className='text-black no-underline'>Search Inventory</a></li>
+          <li><a href="" className='text-black no-underline'>Estimate Payments</a></li>
+          <li><a href="" className='text-black no-underline'>Apply for Financing</a></li>
+          <li><a href="" className='text-black no-underline'>Trade-in Value</a></li>
+          <li><a href="" className='text-black no-underline'>Lease or Finance</a></li>
         </ul>
       </div>
       <div>
-        <h2 className='text-sm'>Discover Honda</h2>
+        <h2 className='text-sm font-bold'>Discover Honda</h2>
         <ul>
-          <li><a href="" className='text-black'>About Us</a></li>
-          <li><a href="" className='text-black'>News & Events</a></li>
-          <li><a href="" className='text-black'>Awards, Accolades & Ratings</a></li>
-          <li><a href="" className='text-black'>Investor Relations</a></li>
-          <li><a href="" className='text-black'>Coperate Social Responsibilty</a></li>
-          <li><a href="" className='text-black'>Honda Financial Services</a></li>
-          <li><a href="" className='text-black'>Honda in America</a></li>
-          <li><a href="" className='text-black'>Honda Worldwide</a></li>
-          <li><a href="" className='text-black'>Honda Merchandise & Apparel</a></li>
+          <li><a href="" className='text-black no-underline'>About Us</a></li>
+          <li><a href="" className='text-black no-underline'>News & Events</a></li>
+          <li><a href="" className='text-black no-underline'>Awards, Accolades & Ratings</a></li>
+          <li><a href="" className='text-black no-underline'>Investor Relations</a></li>
+          <li><a href="" className='text-black no-underline'>Corporate Social Responsibility</a></li>
+          <li><a href="" className='text-black no-underline'>Honda Financial Services</a></li>
+          <li><a href="" className='text-black no-underline'>Honda in America</a></li>
+          <li><a href="" className='text-black no-underline'>Honda Worldwide</a></li>
+          <li><a href="" className='text-black no-underline'>Honda Merchandise & Apparel</a></li>
         </ul>
       </div>
       <div>
-        <h2 className='text-sm'>Help & Support</h2>
+        <h2 className='text-sm font-bold'>Help & Support</h2>
         <ul>
-        <li><a href="" className='text-black'>FAQ</a></li>
-          <li><a href="" className='text-black'>Customer Service</a></li>
-          <li><a href="" className='text-black'>Mobility Assistance Program</a></li>
-          <li><a href="" className='text-black'>Rear Entertainment System Accessibilty for individuals with Disabilities</a></li>
-          <li><a href="" className='text-black'>Buyer's Guide</a></li>
+          <li><a href="" className='text-black no-underline'>FAQ</a></li>
+          <li><a href="" className='text-black no-underline'>Customer Service</a></li>
+          <li><a href="" className='text-black no-underline'>Mobility Assistance Program</a></li>
+          <li><a href="" className='text-black no-underline'>Rear Entertainment System Accessibility for individuals with Disabilities</a></li>
+          <li><a href="" className='text-black no-underline'>Buyer's Guide</a></li>
         </ul>
       </div>
       <div>
-        <h2 className='text-sm'>Owners</h2>
+        <h2 className='text-sm font-bold'>Owners</h2>
         <ul>
-        <li><a href="" className='text-black'>Honda Owners Site</a></li>
-          <li><a href="" className='text-black'>Schedule Service</a></li>
-          <li><a href="" className='text-black'>Parts & Service</a></li>
-          <li><a href="" className='text-black'>Certified Body Shop Locator</a></li>
-          <li><a href="" className='text-black'>eStore</a></li>
-          <li><a href="" className='text-black'>Roadside Assistance</a></li>
-          <li><a href="" className='text-black'>Pay My Bill</a></li>
-          <li><a href="" className='text-black'>Radio & Navi Codes</a></li>
-          <li><a href="" className='text-black'>HondaLink</a></li>
-          <li><a href="" className='text-black'>Wi-FI Hotspot Data Plans</a></li>
-          <li><a href="" className='text-black'>Saftey Recall</a></li>
+          <li><a href="" className='text-black no-underline'>Honda Owners Site</a></li>
+          <li><a href="" className='text-black no-underline'>Schedule Service</a></li>
+          <li><a href="" className='text-black no-underline'>Parts & Service</a></li>
+          <li><a href="" className='text-black no-underline'>Certified Body Shop Locator</a></li>
+          <li><a href="" className='text-black no-underline'>eStore</a></li>
+          <li><a href="" className='text-black no-underline'>Roadside Assistance</a></li>
+          <li><a href="" className='text-black no-underline'>Pay My Bill</a></li>
+          <li><a href="" className='text-black no-underline'>Radio & Navi Codes</a></li>
+          <li><a href="" className='text-black no-underline'>HondaLink</a></li>
+          <li><a href="" className='text-black no-underline'>Wi-FI Hotspot Data Plans</a></li>
+          <li><a href="" className='text-black no-underline'>Safety Recall</a></li>
         </ul>
       </div>
     </div>
